04acea159ae618499ace5e30de207533249ab7e3
Kiran Kumar <kiran.k@cubiclogics.com>
2026-01-06 20:17:09 +0530

    Append file tested

--- BasicNodeOp/Concepts_text_file/03_Concept_3/03_cc.js
+++ BasicNodeOp/Concepts_text_file/03_Concept_3/03_cc.js
@@ -1,30 +1,9 @@
 const fs=require('fs');
-// it return and handle the try and catch or err from the node
-// This is the sync menthod provide by the node and it is blocking function
-const fileData=fs.readFileSync('./BasicNodeOp/Concepts_text_file/03_Concept_3/test.txt','utf-8');
-console.log(fileData);
 
-// const fileData1=fs.readFile('./BasicNodeOp/Concepts_text_file/03_Concept_3/test.txt','utf-8');
-//# the above one give the error 
-// node:internal/errors:540
-//       throw error;
-//       ^
-// TypeError [ERR_INVALID_ARG_TYPE]: The "cb" argument must be of type function. Received type string ('utf-8')
-
-// -------------- use asyc methodology to handle these
-fs.readFile('./BasicNodeOp/Concepts_text_file/03_Concept_3/test.txt','utf-8',(err, res)=>{
-if(err){
-    console.log("Error catched", err);
-}else{
-    console.log("result of the file", res);
-}
-});
-
-// The above function will not return anything and it should be handled with passing a call back functions
-// ------------- Get the out like this in console
-// AzureAD+KiranKumar@KIRAN-K-D-L5420 MINGW64 /d/Tut (aru)
-// $ node BasicNodeOp/Concepts_text_file/03_Concept_3/03_cc.js
-// Hello Node.js
-// My num :0987654321
-// result of the file Hello Node.js
-// My num :0987654321
+// append the file data
+// parameters are 'path of the file and the content of the file
+fs.appendFileSync('./BasicNodeOp/Concepts_text_file/03_Concept_3/test.txt',`Hey Updated ${new Date()} \n`,);
+// code will update like this
+// My num :0987654321Hey Updated Tue Jan 06 2026 20:14:50 GMT+0530 (India Standard Time) 
+// Hey Updated Tue Jan 06 2026 20:15:03 GMT+0530 (India Standard Time) 
+// Hey Updated Tue Jan 06 2026 20:15:09 GMT+0530 (India Standard Time) 
